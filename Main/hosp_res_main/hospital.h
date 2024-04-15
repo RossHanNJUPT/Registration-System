f6d1227d26441fc07de74fb166b6474290d95a75
RossHanNJUPT <145919447+RossHanNJUPT@users.noreply.github.com>
2024-04-15 15:43:16 +0800

    create main prog & ui prog

---
+++ Main/hosp_res_main/hospital.h
@@ -0,0 +1,9 @@
+#ifndef HOSPITAL_H_INCLUDED
+#define HOSPITAL_H_INCLUDED
+#include <windows.h>
+#include <ctime>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
