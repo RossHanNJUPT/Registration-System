23f3e16c20f92aedb5c748d9fabc5caf2345e0d1
RossHanNJUPT <145919447+RossHanNJUPT@users.noreply.github.com>
2024-04-15 21:02:51 +0800

    update

---
+++ Main/hosp_res_main/DocArrange.cpp
@@ -0,0 +1,11 @@
+#include "DocArrange.h"
+using namespace std;
+
+DocArrange::DocArrange()
+{
+}
+
+DocArrange::DocArrange(int DocID, string DocName, string DocDepartment, string DocPos, bool DocState, string DocIntro)
+{
+
+}
---
+++ Main/hosp_res_main/Register.h
@@ -0,0 +1,16 @@
+#ifndef CREGISTER_H
+#define CREGISTER_H
+
+#define ADMIN 1
+#define USER 2
+#define DOCTOR 3
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Register
+{
+public:
+
+};
