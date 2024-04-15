d8a5fb7c20e4fe05fbf164040e1616cbe9fe2e44
RossHanNJUPT <145919447+RossHanNJUPT@users.noreply.github.com>
2024-04-15 17:11:36 +0800

    update

--- Main/hosp_res_main/User.h
+++ Main/hosp_res_main/User.h
@@ -3,6 +3,7 @@
 
 #define ADMIN 1		//管理员
 #define USER 2		//用户
+#define DOCTOR 3	//医生
 
 #include<string>
 #include<iostream>
@@ -12,15 +13,15 @@ class User
 {
 public:
 	User();			//默认构造
-	User(string phoneNum, string pwd, int role, string sfzID, string name);		//带参构造
+	User(string phoneNum, string pwd, int role, string IDNum, string name);		//带参构造
 	~User();		//析构
 	string getName();			//获取用户名字
 	string getPwd();			//获取用户密码
 	int getRole();				//获取用户身份
-	string getSfzID();			//获取身份证号码
+	string getIDNum();			//获取身份证号码
 	string getphoneNum();		//获取手机号码 用户id(账号)
 	void setPwd(string pwd);	//修改密码
-	void setSfzID(string id);	//修改身份证
+	void setIDNum(string id);	//修改身份证
 	void setName(string name);	//修改名字
 protected:
 	string name;		//用户名字（中文）
@@ -28,6 +29,41 @@ protected:
 	int role;			//用户身份
 	string IDNum;		//身份证号码
 	string phoneNum;	//手机号码 用户id(账号)
+};
+
+class Admin
+{
+public:
+	Admin();
+	Admin(string UsrName, string pwd);
+	~Admin();
+	string getUserName();
+	string getPwd();
+	void setUserName();
+	void setPwd();
+protected:
+	string UsrName;
+	string pwd;
+};
+
+class DocArrange
+{
+public:
+	DocArrange();
+	DocArrange(int DocID, string DocName, string DocDepartment, string DocPos, bool DocState, string DocIntro);
+	~DocArrange();
+	int getDocID();
+	string getDocName();
+	string getDocDepartment();
+	string getDocPos();
+	bool getDocState();
+	string getDocIntro();
+	void setDocID();
+	void setDocName();
+	void setDocDepartment();
+	void setDocPos();
+	void setDocState();
+
 };
 
 #endif
